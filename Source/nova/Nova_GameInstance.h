7320c7a3f05eb38298ed37473bd767d9a147900c
MestreSol <mb.muitobosta@gmail.com>
2024-12-20 21:10:41 -0300

    Add CPP code

---
+++ Source/nova/Nova_GameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Nova_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOVA_API UNova_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
